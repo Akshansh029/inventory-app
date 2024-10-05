b56f21df20522996ec15b8f7493cdccfb16c8379
akshanshsingh00 <akshanshsingh00@gmail.com>
2024-10-05 10:12:52 +0530

    Put schema in frontend for validation

---
+++ server/src/index.ts
@@ -0,0 +1,26 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import helmet from "helmet";
+import morgan from "morgan";
+// ROUTE IMPORTS
+import dashboardRoutes from "./routes/dashaboardRoutes";
+
+// CONFIGURATIONS
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+// ROUTES
+app.use("/dashboard", dashboardRoutes);
+
+// SERVER
+const port = process.env.port || 3001;
+app.listen(port, () => console.log(`Server running on port ${port}`));
