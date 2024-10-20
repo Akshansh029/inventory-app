e17d48899275e49dd5ab0b709faab0eb9f198b79
Akshansh <akshanshsingh00@gmail.com>
2024-10-20 22:48:17 +0530

    Making Products page

---
+++ client/src/app/products/CreateProductModal.tsx
@@ -0,0 +1,78 @@
+import React, { ChangeEvent, FormEvent, useState } from "react";
+import { v4 } from "uuid";
+import Header from "../(components)/Header";
+
+type ProductFormData = {
+  name: string;
+  price: number;
+  stockQuantity: number;
+  rating: number;
+};
+
+type CreateProductModalProps = {
+  isOpen: boolean;
+  onClose: () => void;
+  onCreate: (formData: ProductFormData) => void;
+};
+
+const CreateProductModal = ({
+  isOpen,
+  onClose,
+  onCreate,
+}: CreateProductModalProps) => {
+  const [formData, setFormData] = useState({
+    productId: v4(),
+    name: "",
+    price: 0,
+    stockQuantity: 0,
+    rating: 0,
+  });
+
+  if (!isOpen) {
+    return null;
+  }
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    onCreate(formData);
+    onClose();
+  };
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]:
+        name === "price" || name === "stockQuantity" || name === "rating"
+          ? parseFloat(value)
+          : value,
+    });
+  };
+
+  const labelCssStyles = "block text-sm font-medium text-gray-700";
+  const inputCssStyles =
+    "block w-full mb-2 p-2 text-gray-500 border-2 rounded-md";
+
+  return (
+    <div className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full z-20">
+      <div className="relative top-20 mx-auto p-5 border w-96 shadow-lg rounded-md bg-white">
+        <Header name="Create new product" />
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="productName" className={labelCssStyles}>
+            Product Name
+          </label>
+          <input
+            type="text"
+            name="name"
+            placeholder="Name"
+            onChange={handleChange}
+            value={formData.name}
+            className={inputCssStyles}
+          />
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default CreateProductModal;
